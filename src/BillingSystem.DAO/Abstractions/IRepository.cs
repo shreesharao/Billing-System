d33a3bfaf3fe2a36501903228d0335ccf04c1d6d
shreesharao <sshreesha4@gmail.com>
2018-01-11 18:42:10 +0530

    ORM Data Access object added

---
+++ src/BillingSystem.DAO/Abstractions/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem.DAO.Abstractions
+{
+    interface IRepository<TEntity> where TEntity:class
+    {
+        //making the methods virtual so that they can be overrriden later
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy,
+            string includeProperties);
+
+        TEntity GetById(object Id);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(object Id);
+
+        void Delete(TEntity entity);
+
+    }
+}
---
+++ src/BillingSystem.DAO/CommonRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using BillingSystem.DAO.Abstractions;
+
+
+namespace BillingSystem.DAO
+{
+    public class CommonRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+
+        public virtual IEnumerable<TEntity> Get(Expression<System.Func<TEntity, bool>> filter, System.Func<System.Linq.IQueryable<TEntity>, System.Linq.IOrderedQueryable<TEntity>> orderBy, string includeProperties)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual TEntity GetById(object Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void Delete(object Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
